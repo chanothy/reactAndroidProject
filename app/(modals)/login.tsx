8b1b4ce8e281c77e701f256e219d2c26e2fd8f15
timchan <timchan@iu.edu>
2023-12-04 15:56:42 -0500

    login exists

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,10 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  Button,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState } from "react";
+import { FIREBASE_AUTH } from "../../FirebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
-export default function Login() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error: any) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      alert("Check your emails!");
+    } catch (error: any) {
+      console.log(error);
+      alert("Registration failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        {loading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <>
+            <Button style={styles.button} title="Login" onPress={signIn} />
+            <Button
+              style={styles.button}
+              title="Create account"
+              onPress={signUp}
+            />
+          </>
+        )}
+      </KeyboardAvoidingView>
     </View>
-  )
-}
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+  },
+});
